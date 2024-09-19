07312f49929c159a7abd8154c7a1af239fd6af3c
Carlos Rivera <cr4185320@gmail.com>
2024-09-19 14:58:53 -0500

    Update build.bat

--- build.bat
+++ build.bat
@@ -9,10 +9,14 @@ if not exist bin mkdir bin
 set WARNING_FLAGS=/W4 /wd4100 /wd4101 /wd4189 /wd4201 /wd4456 /wd4505 /wd4706
 set INCLUDES=/Isrc\ /Iext\ /Iext\freetype\include\
 set COMMON_COMPILER_FLAGS=/nologo /FC /EHsc /Fdbin\ /Fobin\ %WARNING_FLAGS% %INCLUDES%
+set COMMON_LINKER_FLAGS=/INCREMENTAL:NO /OPT:REF
 
+set LIBS=/LIBPATH:ext\freetype\libs\x64\Release\ user32.lib shell32.lib kernel32.lib winmm.lib shlwapi.lib freetype.lib
 set COMPILER_FLAGS=%COMMON_COMPILER_FLAGS%
+set LINKER_FLAGS=%COMMON_LINKER_FLAGS% %LIBS%
 if %MODE%==release (
   set COMPILER_FLAGS=/O2 /D %COMPILER_FLAGS%
+  set LINKER_FLAGS=/SUBSYSTEM:WINDOWS /ENTRY:mainCRTStartup %LINKER_FLAGS%
 ) else if %mode%==debug (
   set COMPILER_FLAGS=/Zi /Od %COMPILER_FLAGS%
 ) else (
@@ -20,11 +24,5 @@ if %MODE%==release (
   exit /B 2
 )
 
-rem set COMPILER_FLAGS=%COMPILER_FLAGS% /DHOC_ENTRY_WIN32
-set LIBS=/LIBPATH:ext\freetype\libs\x64\Release\ user32.lib shell32.lib kernel32.lib winmm.lib shlwapi.lib freetype.lib
-set COMMON_LINKER_FLAGS=/INCREMENTAL:NO /OPT:REF
-set LINKER_FLAGS=%COMMON_LINKER_FLAGS% %LIBS%
-
-rem CL %COMPILER_FLAGS% src\meta\meta_generator.cpp /Femeta_generator.exe /link %COMMON_LINKER_FLAGS%
-
+::CL %COMPILER_FLAGS% src\meta\meta_generator.cpp /Femeta_generator.exe /link %COMMON_LINKER_FLAGS%
 CL %COMPILER_FLAGS% src\pix3\pix3_main.cpp /FePix3.exe /link %LINKER_FLAGS%
